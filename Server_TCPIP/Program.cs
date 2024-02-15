6db11f81b69fd1c27f941053dc6b00b6291a5a71
Larry <dongsu311@gmail.com>
2024-02-15 23:38:59 +0900

    First Commit

---
+++ Server_TCPIP/Program.cs
@@ -0,0 +1,126 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server_TCPIP
+{
+    internal class Program
+    {
+        public Dictionary<TcpClient, string> clientList = new Dictionary<TcpClient, string>();
+        //각 클라이언트마다 리스트에 추가
+
+        TcpListener server = null;
+        TcpClient clientSocket = null;
+        static int counter = 0; // 사용자 수
+        string date; // 날짜
+
+        static void Main(string[] args)
+        {
+            // 쓰레드 생성
+            Thread thread = new Thread(InitSocket);
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void InitSocket()
+        {
+            server = new TcpListener(IPAddress.Any, 1000); // 서버 접속, IP, Port
+            clientSocket = default(TcpClient); // 소켓 설정
+            server.Start();
+            Console.WriteLine(">> Server Started");
+
+            while (true)
+            {
+                try
+                {
+                    counter++; // 클라이언트 수 증가
+                    clientSocket = server.AcceptTcpClient();
+                    Console.WriteLine(">> Accept connection from Client");
+
+                    NetworkStream stream = clientSocket.GetStream();
+                    byte[] buffer = new byte[1024]; // 버퍼
+                    int bytes = stream.Read(buffer, 0, buffer.Length);
+                    string user_name = Encoding.UTF8.GetString(buffer, 0, bytes);
+                    user_name = user_name.Substring(0, user_name.IndexOf("$")); // Client 사용자명
+
+                    clientList.Add(clientSocket, user_name); // client 리스트에 추가
+                    SendMessage(user_name + "님이 입장하셨습니다.", "", false);
+                    // 모든 클라이언트에 메시지 전송
+
+                    handleClient h_client = new handleClient(); // 클라이언트 추가
+                    h_client.OnReceived += new handleClient.MessageDisplayHandler(OnReceived);
+                    h_client.OnDisconnected += new handleClient.DisconnectedHandler(handleClient_OnDisconnected);
+                    h_client.startClient(clientSocket, clientList);
+                }
+
+                catch (SocketException se)
+                {
+                    Trace.WriteLine(string.Format("InitSocket - Exception : {0}", se.Message));
+                    break;
+                }
+
+                catch (Exception ex)
+                {
+                    Trace.WriteLine(string.Format("InitSocket - Exception : {0}", ex.Message));
+                    break;
+                }
+            }
+            clientSocket.Close(); // client 소켓 닫기
+            server.Stop(); // 서버 종료
+        }
+
+        void handleClient_OnDisconnected(TcpClient clientSocket) // 클라이언트 접속 해제 핸들러
+        {
+            if (clientList.ContainsKey(clientSocket))
+            {
+                clientList.Remove(clientSocket);
+            }
+        }
+
+        private void OnReceived(string message, string user_name) // 클라이언트로부터 받은 데이터
+        {
+            if (message.Equals("leaveChat"))
+            {
+                string DisplayMessage = "leave user : " + user_name;
+                Console.WriteLine(DisplayMessage);
+                SendMessage("leaveChat", user_name, true);
+            }
+            else
+            {
+                string DisplayMessage = "From client : " + user_name + " : " + message;
+                Console.WriteLine(DisplayMessage);
+                SendMessage("leaveChat", user_name, true); // 모든 Client에게 전송
+            }
+        }
+
+        public void SendMessage(string message, string user_name, bool flag)
+        {
+            foreach (var pair in clientList)
+            {
+                date = DateTime.Now.ToString("yyyy.MM.dd. HH:mm:ss"); // 현재 날짜 받기
+
+                TcpClient client = pair.Key as TcpClient;
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = null;
+
+                if (flag)
+                {
+                    if (message.Equals("leaveChat"))
+                        buffer = Encoding.UTF8.GetBytes(user_name + "님이 대화방을 나갔습니다.");
+                    else
+                        buffer = Encoding.UTF8.GetBytes("[ " + date + " ] " + user_name + " : " + message);
+                }
+                else
+                {
+                    buffer = Encoding.UTF8.GetBytes(message);
+                }
+
+                stream.Write(buffer, 0, buffer.Length); // 버퍼 쓰기
+                stream.Flush();
+            }
+        }
+    }
+}
---
+++ Server_TCPIP/handleClient.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_TCPIP
+{
+    internal class handleClient
+    {
+        TcpClient clientSocket = null;
+        public Dictionary<TcpClient, string> clientList = null;
+
+        public void startClient(TcpClient clientSocket, Dictionary<TcpClient, string> clientList)
+        {
+            this.clientSocket = clientSocket;
+            this.clientList = clientList;
+
+            Thread thread_handler = new Thread(doChat);
+            thread_handler.IsBackground = true;
+            thread_handler.Start();
+        }
+
+        public delegate void MessageDisplayHandler(string message, string user_name);
+        public event MessageDisplayHandler OnReceived;
+
+        public delegate void DisconnectedHandler(TcpClient clientSocket);
+        public event DisconnectedHandler OnDisconnected;
+
+        private void doChat() 
+        {
+            NetworkStream stream = null;
+            try
+            {
+                byte[] buffer = new byte[1024];
+                string message = string.Empty;
+                int bytes = 0;
+                int MessageCount = 0;
+
+                while (true)
+                {
+                    MessageCount++;
+                    stream = clientSocket.GetStream();
+                    bytes = stream.Read(buffer, 0, buffer.Length);
+                    message = Encoding.UTF8.GetString(buffer, 0, bytes);
+                    message = message.Substring(0, message.IndexOf("$"));
+
+                    if(OnReceived != null)
+                    {
+                        OnReceived(message, clientList[clientSocket].ToString());
+                    }
+                }
+            }
+            catch (SocketException se)
+            {
+                Trace.WriteLine(string.Format("doChat - SocketException : {0}", se.Message));
+                if (clientSocket != null)
+                {
+                    if(OnDisconnected != null)
+                    {
+                        OnDisconnected(clientSocket);
+
+                        clientSocket.Close();
+                        stream.Close();
+                    }
+                }
+            }
+        }
+    }
+}
